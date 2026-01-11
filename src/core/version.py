fb55e6a4e7abc43f8a15288fdcfe8219d96617cc
logando-al <luqmann1972@gmail.com>
2026-01-11 13:39:25 +0800

    chore: bump version to 0.2.0

--- src/core/version.py
+++ src/core/version.py
@@ -1,7 +1,7 @@
 """
 BiblioFlow - Intelligent Research PDF Organizer
 """
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 APP_NAME = "BiblioFlow"
 GITHUB_REPO = "logando-al/biblioFlow"
 GITHUB_API_URL = f"https://api.github.com/repos/{GITHUB_REPO}/releases/latest"
